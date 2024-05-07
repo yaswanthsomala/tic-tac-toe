dbd0978bd5d48f4308d27eab4f0201bd97ba024b
yaswanthsomala <somalayaswanth@gmail.com>
2024-05-07 16:47:47 +0530

    changes

--- src/Info.js
+++ src/Info.js
@@ -4,7 +4,7 @@ import "./css/info.css";
 const Info = () => {
   return (
     <div className="info">
-      <div className="player">Player 0: X</div>
+      <div className="player">Player 1: X</div>
       <div className="player">Player 2: O</div>
     </div>
   );
