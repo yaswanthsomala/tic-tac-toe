1448dc0e6e5f0ff27113cb00ddacf8c6725d5364
yaswanthsomala <somalayaswanth@gmail.com>
2024-05-07 16:25:39 +0530

    changes

--- src/Info.js
+++ src/Info.js
@@ -2,12 +2,12 @@
 import "./css/info.css";
 
 const Info = () => {
-	return (
-		<div className="info">
-			<div className="player">Player 1: X</div>
-			<div className="player">Player 2: O</div>
-		</div>
-	)
-}
+  return (
+    <div className="info">
+      <div className="player">Player 0: X</div>
+      <div className="player">Player 2: O</div>
+    </div>
+  );
+};
 
 export default Info;
